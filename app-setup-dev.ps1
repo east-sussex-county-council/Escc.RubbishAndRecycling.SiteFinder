905f0828a0bb03c96fd85c83a7976de97bbe6051
Rick Mason <richard.mason@eastsussex.gov.uk>
2016-03-09 17:46:43 +0000

    Update framework version and remote template location, and remove obsolete dependencies

--- app-setup-dev.ps1
+++ app-setup-dev.ps1
@@ -40,7 +40,6 @@ DownloadProjectIfMissing $parentFolderOfThisScript "Escc.EastSussexGovUK"
 DownloadProjectIfMissing $parentFolderOfThisScript "Escc.NavigationControls"
 DownloadProjectIfMissing $parentFolderOfThisScript "Escc.Data.Web"
 DownloadProjectIfMissing $parentFolderOfThisScript "Escc.HouseStyle"
-DownloadProjectIfMissing $parentFolderOfThisScript "Escc.Data.ActiveDirectory"
 
 EnableDotNet40InIIS
 CreateApplicationPool $projectName
